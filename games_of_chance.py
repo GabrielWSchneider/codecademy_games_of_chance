edd0e3ec926756bfe399d1a28aa7c1f725ede638
Gabriel W. Schneider <70598846+GabrielWSchneider@users.noreply.github.com>
2020-09-12 13:48:46 -0500

    Req. #4: Build a function that simulates drawing a card versus another player.

--- games_of_chance.py
+++ games_of_chance.py
@@ -53,6 +53,38 @@ def cho_han(player_bet, player_choice):
 	
 	return cash_difference
 
+def card_versus(player_bet):
+	cash_difference = 0
+	player_won = False						# The player hasn't won yet.
+	
+	card_deck = []
+	for i in range(4):						# Four suits...
+		for j in range(1, 14):				# 13 cards per suit...
+			card_deck.append(j)
+	random.shuffle(card_deck)				# Shuffle the new deck.
+	
+	# Each player draws a card.
+	player_1_card = card_deck.pop(random.choice(range(len(card_deck))))
+	player_2_card = card_deck.pop(random.choice(range(len(card_deck))))
+	print("\nYou drew {}.\nYour opponent drew {}.".format(player_1_card, player_2_card))
+	
+	if player_1_card > player_2_card:
+		print("You beat Player 2!")
+		player_won = True
+	elif player_1_card < player_2_card:
+		print("You lost to Player 2.")
+	else:
+		print("You tied with Player 2!")
+		return cash_difference				# Player doesn't win or lose cash.
+	
+	if player_won:
+		cash_difference = player_bet
+	else:
+		cash_difference = player_bet * -1
+	
+	return cash_difference
+	
+
 # Call your game of chance functions here
 
 # For-loop calls coin_flip() X times.
@@ -65,10 +97,15 @@ def cho_han(player_bet, player_choice):
 #	print("You got ${} by guessing {}!".format(coin_flip(100, guess), guess))
 
 # For-loop calls cho_han() Y times.
-Y = 6
-for i in range(Y):
-	if bool(random.randint(0, 1)):
-		guess = "Even"
-	else:
-		guess = "Odd"
-	print("You got ${} by guessing {}!".format(cho_han(100, guess), guess))
+#Y = 6
+#for i in range(Y):
+#	if bool(random.randint(0, 1)):
+#		guess = "Even"
+#	else:
+#		guess = "Odd"
+#	print("You got ${} by guessing {}!".format(cho_han(100, guess), guess))
+
+# For-loop calls card_versus() Z times.
+Z = 6
+for i in range(Z):
+	print("You got ${} from your opponent!".format(card_versus(100)))

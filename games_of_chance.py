9a4598877533cc8b941584479fddeac577baca00
Gabriel W. Schneider <70598846+GabrielWSchneider@users.noreply.github.com>
2020-09-12 10:02:32 -0500

    Req. #3: Build a function that simulates playing Cho-Han.

--- games_of_chance.py
+++ games_of_chance.py
@@ -26,13 +26,49 @@ def coin_flip(player_bet, player_choice):
 	
 	return cash_difference
 
+def cho_han(player_bet, player_choice):
+	cash_difference = 0
+	player_won = False						# The player hasn't won yet.
+	
+	die1 = random.randint(1, 6)
+	die2 = random.randint(1, 6)
+	sum_of_dice = die1 + die2
+	print("\n{} + {} = {}".format(die1, die2, sum_of_dice))
+	
+	sum_is_even = sum_of_dice % 2 == 0
+	
+	if player_choice == "Even" and sum_is_even:
+		print("Even. Good work!")
+		player_won = True
+	elif player_choice == "Odd" and (not sum_is_even):
+		print("Odd! Nice.")
+		player_won = True
+	else:
+		print("Sorry, chump - the House wins.")
+	
+	if player_won:
+		cash_difference = player_bet
+	else:
+		cash_difference = player_bet * -1
+	
+	return cash_difference
+
 # Call your game of chance functions here
 
-# The following for loop calls coin_flip X times.
-X = 6
-for i in range(X):
+# For-loop calls coin_flip() X times.
+#X = 6
+#for i in range(X):
+#	if bool(random.randint(0, 1)):
+#		guess = "Heads"
+#	else:
+#		guess = "Tails"
+#	print("You got ${} by guessing {}!".format(coin_flip(100, guess), guess))
+
+# For-loop calls cho_han() Y times.
+Y = 6
+for i in range(Y):
 	if bool(random.randint(0, 1)):
-		guess = "Heads"
+		guess = "Even"
 	else:
-		guess = "Tails"
-	print("You got ${} by guessing {}!".format(coin_flip(100, guess), guess))
+		guess = "Odd"
+	print("You got ${} by guessing {}!".format(cho_han(100, guess), guess))
